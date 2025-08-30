e64ffe758cd76a49a8753a3c2b0067d2bb68c3e3
DanielVillalba-dev <132484397+Poker-As@users.noreply.github.com>
2025-08-30 15:51:45 -0500

    Initial with Spring

---
+++ src/main/java/com/merkagrobackend/merkagroBackend/MerkagroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.merkagrobackend.merkagroBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkagroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerkagroBackendApplication.class, args);
+	}
+
+}
